cf48ff652ee6e0c3923bb47d948be6ea0e201744
lozo6 <lorenzo.francisco@stonybrook.edu>
2022-10-05 09:50:30 -0400

    added data

--- cronJob.py
+++ cronJob.py
@@ -1,9 +1,16 @@
 import os
 import time
 import pandas as pd
+import requests as rq
+import json
 
 # assign data to vairable
-df = pd.read_csv('data/food_delivery.csv')
+api = rq.get('https://data.cms.gov/data-api/v1/dataset/f1a8c197-b53d-4c24-9770-aea5d5a97dfb/data')
+api = api.json()
+api = pd.DataFrame(api)
+api.to_csv('data/opiodData.csv')
+
+df = pd.read_csv('data/opiodData.csv')
 
 # get current time
 currentTime = time.time()
